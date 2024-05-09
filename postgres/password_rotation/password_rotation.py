37c167d5f43de088a2c04bf296c7bcc54b2ffeca
GarrettWilkerson <gwilkerson@firstorion.com>
2024-05-09 13:17:51 -0500

    initial commit of db automation code.

---
+++ postgres/password_rotation/password_rotation.py
@@ -0,0 +1,19 @@
+# this script is to call the sql code that will rotate the passwords
+# this script will then update the associated applications/users
+#   - for applications we will simulate this with updating env variables on the server
+#   - for users we will send and email with encrypted? crendentials
+#  this script should be called in some automation tool (ansible, cron?)
+
+
+def rotateAndUpdatePasswords():
+
+    print("done")
+
+
+
+if __name__ == '__main__'():
+    rotateAndUpdatePasswords()
+
+
+
+
---
+++ postgres/password_rotation/rotate_passwords.sql
@@ -0,0 +1,4 @@
+-- this script is a poc to automate password rotation on a postgres database
+-- intended to be called from another automation script
+-- this script sould provide the caller with information needed to update the applications/users
+
